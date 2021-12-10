3b5a58a41666b750dae07c4962e5e966331d0c08
Prakash <prakashmtech1998@gmail.com>
2021-12-10 12:54:57 +0530

    added checkbox component

---
+++ myntra/src/components/UI/atoms/Checkbox/Checkbox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Checkbox = ({CheckboxValue}) => {
+    return(
+        <>
+        <input type="checkbox" value={CheckboxValue}/>
+        </>
+    );
+}
+
+export default Checkbox;
