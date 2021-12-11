2dbf07ff05a7c07686ec20c26ec3c03302826726
Prakash <prakashmtech1998@gmail.com>
2021-12-11 18:04:39 +0530

    added reducer function

---
+++ myntra/src/components/Redux/Reducer.js
@@ -0,0 +1,20 @@
+const Reducer = (state, action) => {
+     switch(action.type){
+         case 'MEN': return {
+
+         };
+         case 'WOMEN': return {
+
+         };
+         case 'BOYS' : return {
+
+         };
+         case 'GIRLS' : return {
+             
+         };
+
+     }
+
+}
+
+export default Reducer;
