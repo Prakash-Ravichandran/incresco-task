31f63266fc6997f5295fbcd319221c9ced1780a3
Prakash <prakashmtech1998@gmail.com>
2021-12-12 18:59:24 +0530

    added redux react-redux support

--- myntra/src/components/UI/atoms/RADIO/RadioComponent.js
+++ myntra/src/components/UI/atoms/RADIO/RadioComponent.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './RadioComponent.css';
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 
 const Radio =({identityName,radioValue, labelValue}) => {
+  const myState = useSelector((State) => State);
+  const dispatch = useDispatch();
     return(
         <>
         <label htmlFor={identityName} name="radio">
-               <input type='radio' name='radio' id={identityName} value={radioValue}  />
+               <input type='radio' 
+                      name='radio' 
+                      id={identityName} 
+                      value={radioValue} 
+                      onClick={() => { 
+                          dispatch({
+                              type: 'MEN'
+                          })
+
+               }}/>
                <span className='align-margin shirt-name-font'>{labelValue}</span>
            </label>
         </>
